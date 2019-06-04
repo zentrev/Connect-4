dd59db0320ac50948171a8d5480e02ecc8415267
littleghostprince <ashleysmejia@gmail.com>
2019-06-04 13:07:47 -0600

    6/4
    menu selection thingy

---
+++ Assets/Scripts/GUI/Selection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Selection: MonoBehaviour
+{
+    TMP_Dropdown m_dropDown;
+    public static int selection;
+
+    public void Start()
+    {
+        m_dropDown = GetComponent<TMP_Dropdown>();
+        m_dropDown.onValueChanged.AddListener(delegate
+        {
+            Onselection(m_dropDown);
+        });
+    }
+    public void Onselection(TMP_Dropdown change)
+    {
+        selection = change.value;
+        
+        Debug.Log(selection);
+    }
+}
