863b1e38417992fbb338fa9a17e83e95256e0ea1
Colby Saenthavy <CSenthavy@student.neumont.edu>
2019-06-05 11:37:05 -0600

    Added "Animations"

---
+++ Assets/Scripts/Animations/StopBounce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopBounce : MonoBehaviour
+{
+    public Rigidbody2D piece;
+
+    float timmer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timmer += Time.deltaTime;
+        if(piece.velocity.y < 0.005 && timmer > 9.0f)
+        {
+            piece.isKinematic = true;
+            piece.velocity = Vector2.zero;
+        }
+        else
+        {
+            piece.isKinematic = false;
+        }
+    }
+}
