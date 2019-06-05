1e004a2d6f6a8e0fc9f07bb4e2947b0e3a02b4d3
littleghostprince <ashleysmejia@gmail.com>
2019-06-05 13:43:10 -0600

    6/5
    added music to menu and to have it stopped when press start . I also added background to game (Feel free to change it if you guys want too)

--- Assets/Scripts/GUI/ChangeLevelButton.cs
+++ Assets/Scripts/GUI/ChangeLevelButton.cs
@@ -6,19 +6,22 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class ChangeLevelButton : MonoBehaviour
 {
-
     [SerializeField] int m_nextLevel = 0;
+    AudioSource MainMenu_Audio = null;
 
     private Button m_button = null;
+
 
     void Start()
     {
         m_button = GetComponent<Button>();
         m_button.onClick.AddListener(TaskOnClick);
+        MainMenu_Audio = GetComponent<AudioSource>();
     }
 
    public void TaskOnClick()
     {
+        if (MainMenu_Audio.isPlaying) MainMenu_Audio.Stop();
         GameManager.Instance.LoadLevel(m_nextLevel);
     }
 }
